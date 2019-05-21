f1ab2e52c260c3fa15ce74b16c9b6fd71edc8465
Michał Łukiański <michallukianski.0@gmail.com>
2019-05-21 10:36:19 +0200

    Add twx communication

---
+++ Arduino-wifi/include/Things.h
@@ -0,0 +1,3 @@
+#define getTagInfo "https://pp-1905210639vk.devportal.ptc.io/Thingworx/Things/OCP/Services/getTagInfo?appKey=a3b09e0a-dd6b-4504-b7ae-ad3e20e7a35c"
+#define sendBattery "https://pp-1905210639vk.devportal.ptc.io/Thingworx/Things/OCP/Services/SetBattery?appKey=a3b09e0a-dd6b-4504-b7ae-ad3e20e7a35c&betterylevel="
+#define AuthorizePhone "https://pp-1905210639vk.devportal.ptc.io/Thingworx/Things/OCP/Services/AuthorizePhone?appKey=a3b09e0a-dd6b-4504-b7ae-ad3e20e7a35c&Tag="
